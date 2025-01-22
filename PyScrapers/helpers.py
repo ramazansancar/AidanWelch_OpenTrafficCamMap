b34657534e88ff75ad8db02cde5d58018df98c71
Ramazan Sancar <me@ramazansancar.com.tr>
2025-01-22 14:06:05 +0300

    fix: maps api fixed.

--- PyScrapers/helpers.py
+++ PyScrapers/helpers.py
@@ -7,7 +7,7 @@ import re
 async def str2latlng(str:str) -> tuple[float, float]:
     oturum  = AsyncClient(headers={"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"}, timeout=15)
     yer_adi = re.sub(r"\s*\d+$", "", unicode_tr(str).title())
-    istek   = await oturum.get(f"https://keyiflerolsun.dev/api/v1/harita?ara={yer_adi}")
+    istek   = await oturum.get(f"https://keyiflerolsun.dev/api/v1/osm?ara={yer_adi}")
 
     try:
         lat, lng = istek.json()["result"].split(",")
